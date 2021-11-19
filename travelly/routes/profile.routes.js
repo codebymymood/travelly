be04a866787fac3e55ae12075be5ff03ce121e6b
jcsfpc <joanaparentedacosta@gmail.com>
2021-11-19 09:40:35 +0000

    jjhgsg

--- travelly/routes/profile.routes.js
+++ travelly/routes/profile.routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const UserModel = require('../models/User.model')
 
+router.get('/profile', (res, req, next) => {
+    res.render('/profile/edit-profile.hbs')
+})
+
 
 
 
