221b0da77db1351ba68e8922ae8aeee3be1db0bf
jcsfpc <joanaparentedacosta@gmail.com>
2021-11-20 19:13:17 +0000

    asd

--- travelly/routes/myTrips.routes.js
+++ travelly/routes/myTrips.routes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 const TripsModel = require('../models/Trips.model')
+const axios = require('axios')
+const myApi = 'https://api.opentripmap.com/0.1'
 
+router.get('/mytrips', (req, res, next) => {
 
+    axios.get(`${myApi}/en/places/bbox?apikey=${process.env.TRIPMAP_KEY}`)
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
 
 
 
