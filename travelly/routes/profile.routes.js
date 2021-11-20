fc4b311b5d8879f5c8ba229ff45343a97a274cd6
jcsfpc <joanaparentedacosta@gmail.com>
2021-11-20 22:56:30 +0000

    fix footer

--- travelly/routes/profile.routes.js
+++ travelly/routes/profile.routes.js
@@ -4,7 +4,7 @@ require('../routes/auth.routes')
 
 router.get('/profile', (req, res, next) => {
     let userInfo = req.session.myProperty
-    res.render('../views/profile/profile.hbs', {name: userInfo.name} , {layout: 'logged-in-layout.hbs'})
+    res.render('../views/profile/profile.hbs', {name: userInfo.name} )
 })
 
 router.get('/profile/edit', (req, res, next) => {
