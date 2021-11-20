26e6b9c2f6ae0041a64c19350f5dbef5392d5afa
Mário Rui <marinhocosta@live.com.pt>
2021-11-20 14:05:54 +0000

    layout login

--- travelly/routes/profile.routes.js
+++ travelly/routes/profile.routes.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const UserModel = require('../models/User.model')
 
 router.get('/profile', (req, res, next) => {
-    res.render('../views/profile/profile.hbs')
+    res.render('../views/profile/profile.hbs', {layout: 'logged-in-layout.hbs'})
 })
 
 router.get('/profile/edit', (req, res, next) => {
-    res.render('../views/profile/edit-profile.hbs')
+    res.render('../views/profile/edit-profile.hbs', {layout: 'logged-in-layout.hbs'})
 })
 
 
