24540b1162a23079739abb2cae29e956a2f5a318
jcsfpc <joanaparentedacosta@gmail.com>
2021-11-21 09:55:56 +0000

    add trip form redirect partially working; needs redirect fixing

--- travelly/routes/profile.routes.js
+++ travelly/routes/profile.routes.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
 const UserModel = require('../models/User.model')
+const FavTrips = require("../models/favTrips.model");
 require('../routes/auth.routes')
 
 router.get('/profile', (req, res, next) => {
     let userInfo = req.session.myProperty
     res.render('../views/profile/profile.hbs', {name: userInfo.name} )
+})
+
+router.post('/profile', (req, res, next) => {
+
+    const {destination, start, end} = req.body
+
+    FavTrips.create({destination, start, end})
+    .then(() => {
+        res.redirect('/profile')
+    })
+    .catch((err) => {
+        next(err)
+    })
+
 })
 
 router.get('/profile/edit', (req, res, next) => {
